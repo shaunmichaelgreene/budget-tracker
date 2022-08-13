e5c7154454ff4a1037159eb70328ae9dd81af5a3
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-08-13 09:46:09 -0400

    indexedDB added for offline functionality

--- public/js/idb.js
+++ public/js/idb.js
@@ -8,4 +8,52 @@ request.onupgradeneeded = function (event) {
 
 request.onsuccess = function (event) {
     db = event.target.result;
+
+    if (navigator.onLine) {
+        uploadTransaction();
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const trans = db.transaction(['new_transaction'], 'readwrite');
+    const transObjectStore = transaction.objectStore('new_transaction');
+    transObjectStore.add(record);
 }
+
+function uploadTransaction() {
+    const trans = db.transaction(['new_transaction'], 'readwrite');
+    const transObjectStore = transaction.objectStore('new_transaction');
+    const getAll = transObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+
+                    const trans = db.transaction(['new_transaction'], 'readwrite');
+                    const transObjectStore = transaction.objectStore('new_transaction');
+                    transObjectStore.clear();
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+}
+
+window.addEventListener('online', uploadTransaction);
