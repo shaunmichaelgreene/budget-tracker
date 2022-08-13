5ea96dabc55363e7bbc9c67543430e2fc8e5e191
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-08-13 11:50:02 -0400

    refactor completed, app fully functional

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,18 @@
-const CACHE_NAME = "my-site-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v2";
+const CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
     '/',
-    '/index.html',
-    '/manifest.json',
-    '/service-worker.js',
-    '/assets/css/styles.css',
-    '/assets/images/icons/icon-72x72.png',
-    '/assets/images/icons/icon-96x96.png',
-    '/assets/images/icons/icon-128x128.png',
-    '/assets/images/icons/icon-144x144.png',
+    './index.html',
+    './manifest.json',
+    './js/idb.js',
+    './js/index.js',
+    './service-worker.js',
+    './assets/css/styles.css',
+    './assets/images/icons/icon-72x72.png',
+    './assets/images/icons/icon-96x96.png',
+    './assets/images/icons/icon-128x128.png',
+    './assets/images/icons/icon-144x144.png',
     "./assets/images/icons/icon-152x152.png",
     "./assets/images/icons/icon-192x192.png",
     "./assets/images/icons/icon-384x384.png",
@@ -19,7 +21,7 @@ const FILES_TO_CACHE = [
 
 self.addEventListener('install', function(evt) {
     evt.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
+        caches.open(CACHE_NAME).then((cache) => {
             console.log('Your files have been successfully pre-cached!');
             return cache.addAll(FILES_TO_CACHE);
         })
@@ -29,9 +31,9 @@ self.addEventListener('install', function(evt) {
 
 self.addEventListener('activate', function(evt) {
     evt.waitUntil(
-        caches.keys().then(keyList => {
+        caches.keys().then((keyList) => {
             return Promise.all(
-                keyList.map(key => {
+                keyList.map((key) => {
                     if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
                         console.log('Removing old cached data now', key);
                         return caches.delete(key);
@@ -48,9 +50,9 @@ self.addEventListener('fetch', function(evt) {
         evt.respondWith(
             caches
                 .open(DATA_CACHE_NAME)
-                .then(cache => {
+                .then((cache) => {
                     return fetch(evt.request)
-                        .then(response => {
+                        .then((response) => {
                             //if response is OK, clone and cache
                             if (response.status === 200) {
                                 cache.put(evt.request.url, response.clone());
@@ -68,7 +70,7 @@ self.addEventListener('fetch', function(evt) {
     }
     evt.respondWith(
         fetch(evt.request).catch(function() {
-            return caches.match(evt.request).then(function(response) {
+            return caches.match(evt.request).then(function (response) {
                 if (response) {
                     return response;
                 } else if (evt.request.headers.get('accept').includes('text/html')) {
