e41cd3ada187199ec17315e5e82a7158abf0c647
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-08-13 09:31:25 -0400

    file structure reorganized, adding idb

---
+++ public/js/idb.js
@@ -0,0 +1,11 @@
+let db;
+const request = indexedDB.open('budget-tracker', 1);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+}
